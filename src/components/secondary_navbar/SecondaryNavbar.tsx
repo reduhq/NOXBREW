c17ddf11a1da5b807cdd20341861464681c93fa0
Rey Halsall <reduhq25@gmail.com>
2024-05-14 22:02:56 -0600

    Adding colors to the active section in the secondary navbar

--- src/components/secondary_navbar/SecondaryNavbar.tsx
+++ src/components/secondary_navbar/SecondaryNavbar.tsx
@@ -1,15 +1,21 @@
+'use client'
+
 import { IconHeart, IconHistory, IconShoppingCart, IconSmartHome } from "@tabler/icons-react"
 import styles from './secondary_navbar.module.css'
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 
 export const SecondaryNavbar = () => {
+    const pathname = usePathname()
+
     return (
         <div className={styles.navbar}>
-            <Link href={`/`} className={styles.navbar__icon}><IconSmartHome color="#00ffd1" size={'auto'}/></Link>
-            <Link href={`/cart`} className={styles.navbar__icon}><IconShoppingCart color="#fff" size={'auto'}/></Link>
-            <Link href={`/favorites`} className={styles.navbar__icon}><IconHeart color="#fff" size={'auto'}/></Link>
-            <Link href={`/history`} className={styles.navbar__icon}><IconHistory color="#fff" size={'auto'}/></Link>
+            <Link href={`/`} className={styles.navbar__icon}><IconSmartHome color={pathname=="/"?"#00ffd1":"#fff"} size={'auto'}/></Link>
+            <Link href={`/cart`} className={styles.navbar__icon}><IconShoppingCart color={pathname=="/cart"?"#00ffd1":"#fff"} size={'auto'}/></Link>
+            <Link href={`/favorites`} className={styles.navbar__icon}><IconHeart color={pathname=="/favorites"?"#00ffd1":"#fff"} size={'auto'}/></Link>
+            <Link href={`/history`} className={styles.navbar__icon}><IconHistory color={pathname=="/history"?"#00ffd1":"#fff"} size={'auto'}/></Link>
         </div>
     )
 }
+"#00ffd1"
