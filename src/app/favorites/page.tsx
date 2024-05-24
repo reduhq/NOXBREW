8eaca34950ecc65e91cc5474f89d05a15d99290c
Rey Halsall <reduhq25@gmail.com>
2024-05-24 14:13:35 -0600

    footer finished

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -32,7 +32,6 @@ export default function Page(){
                     />
                 ))}
             </div>
-
         </div>
     )
 }
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,26 @@
+import { IconBrandFacebook, IconBrandInstagram, IconBrandLinkedin, IconBrandWhatsapp, IconBrandX } from '@tabler/icons-react'
 import styles from './footer.module.css'
+import Link from 'next/link'
 
 export const Footer = () => {
     return (
-        <div className={styles.footer}>Footer</div>
+        <div className={styles.footer}>
+            <div className={styles.footer__social}>
+                <div className={styles.social__separador}></div>
+                <div className={styles.social__redes}>
+                    <div><IconBrandFacebook size={30} stroke={1}/></div>
+                    <div><IconBrandInstagram size={30} stroke={1}/></div>
+                    <div><IconBrandWhatsapp size={30} stroke={1}/></div>
+                    <div><IconBrandX size={30} stroke={1}/></div>
+                    <div><IconBrandLinkedin size={30} stroke={1}/></div>
+                </div>
+                <div className={styles.social__separador}></div>
+            </div>
+            <div className={styles.footer__brand}>
+                <Link href={"/"}>NOXBREW</Link>
+            </div>
+            <p className={styles.footer__copyright}>Copyright © {new Date().getFullYear()} • NOXBREW</p>
+            <p className={styles.footer__legal}><Link href={"#"}>Información legal</Link> | <Link href={"#"}>Politica de privacidad</Link></p>
+        </div>
     )
 }
