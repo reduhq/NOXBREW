9a9e2fe233e616cdd3efa3ada30412799ba338eb
Rey Halsall <reduhq25@gmail.com>
2024-06-03 21:55:07 -0600

    Adding a sub-menu when the user is authenticated

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,14 @@
+'use client'
+
 import Image from "next/image"
 import styles from "./navbar.module.css"
 import {IconUser} from "@tabler/icons-react"
 import Link from "next/link"
+import { useAuthStore } from "@/store/auth"
 
 export const Navbar = () => {
+    const {token} = useAuthStore()
+
     return (
         <div className={styles.navbar}>
             <div className="container">
@@ -13,9 +18,30 @@ export const Navbar = () => {
                     </Link>
                     <Link href={"/"}>NOXBREW</Link>
                 </div>
-                <Link href={'/login'} className={styles.user}>
-                    <IconUser color="#fff"/>
-                </Link>
+                {
+                    token?
+                    <div className={`${styles.user} ${styles.auth}`}>
+                        <div className={styles.user_photo}>
+                            <Image alt="user-photo" src={'/user-photo.jpg'} width={500} height={500}/>
+                        </div>
+                        <div className={`${styles.options}`}>
+                            <div className={styles.invisible_item}></div>
+                            <Link href={'#'} className={styles.options__item}>Perfil</Link>
+                            <Link href={'#'} className={styles.options__item}>Cerrar sesión</Link>
+                        </div>
+                    </div>
+                    :
+                    <div className={styles.user}>
+                        <Link href={'/login'}><IconUser color="#fff"/></Link>
+                    </div>
+                }
+                {/* <div className={styles.user}>
+                    {
+                        token?(
+                        )
+                        :<Link href={'/login'}><IconUser color="#fff"/></Link>
+                    }
+                </div> */}
             </div>
         </div>
     )
