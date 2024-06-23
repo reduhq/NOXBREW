f66921e23d34294457533821c9d2e4c67924500f
Rey Halsall <reduhq25@gmail.com>
2024-06-22 18:01:26 -0600

    Fixing the URL when a drink is called

--- src/components/product_card/Product_card.tsx
+++ src/components/product_card/Product_card.tsx
@@ -69,12 +69,12 @@ export const Product_card = ({drink_id, product_name, price, image, description,
                     <h2 className={styles['info-title']}>{product_name}</h2>
                     {fav?<IconHeartFilled size={30} stroke={2} color="#00FFD1" onClick={()=>favHandler(false)}/>:<IconHeart stroke={2} size={30} color="#00FFD1" onClick={()=>favHandler(true)}/>}
                 </div>
-                <Link href={`/${product_name.toLowerCase().replace(" ", "-")}`}  className={styles.card__info}>
+                <Link href={`/${product_name.toLowerCase().replace(/\s+/g, "-")}`}  className={styles.card__info}>
                     <p className={styles['info-description']}>{description}</p>
                     <h3 className={styles['info-price']}><span>$</span>{price.toFixed(2)}</h3>
                 </Link>
             </div>
-            <Link href={`/${product_name.toLowerCase().replace(" ", "-")}`} className={styles.card__image}>
+            <Link href={`/${product_name.toLowerCase().replace(/\s+/g, "-")}`} className={styles.card__image}>
                 <Image
                     src={`${image}`}
                     alt={product_name}
